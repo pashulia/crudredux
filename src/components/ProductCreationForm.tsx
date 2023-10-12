00b3fa0746d9117b359c8db6b476bd77cbe7fda5
Pavel Khegay <pashulia.pk@gmail.com>
2023-10-12 15:27:51 +0600

    add more comment

--- src/components/ProductCreationForm.tsx
+++ src/components/ProductCreationForm.tsx
@@ -15,6 +15,14 @@ const ProductCreationForm: React.FC<ProductCreationFormProps> = ({ onSubmit }) =
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
+    // FormEvent - это событие, которое происходит при отправке формы (например,
+    // при нажатии кнопки "Submit" в форме HTML). Он представляет собой событие,
+    // связанное с HTML-формой и может быть использовано в React или чистом JavaScript
+    // для обработки отправки формы.
+    // FormEvent предоставляет информацию о событии отправки формы, такую как:
+    // event.target: Ссылка на элемент <form>, к которому применяется событие.
+    // event.preventDefault(): Метод, который может быть вызван для предотвращения
+    // стандартного поведения отправки формы (например, перезагрузки страницы).
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         onSubmit({ title, description });
